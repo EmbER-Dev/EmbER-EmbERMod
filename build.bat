44c0ce3dc0c44c25e024b129795d6b49f45012af
croniccorey <cronmod.dev@gmail.com>
2015-06-27 01:24:07 -0400

    Overhaul [Part 2/2]

--- build.bat
+++ build.bat
@@ -1,24 +1,24 @@
 @echo off
 ECHO ----------------------------------------
-echo Creating Confluence Build Folder
-IF Exist ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.confluence rmdir ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.confluence /S /Q
-md ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.confluence\media\
+echo Creating EmbERMod Build Folder
+IF Exist ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.embermod rmdir ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.embermod /S /Q
+md ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.embermod\media\
 
 Echo .svn>exclude.txt
 Echo Thumbs.db>>exclude.txt
 Echo Desktop.ini>>exclude.txt
 Echo dsstdfx.bin>>exclude.txt
 Echo build.bat>>exclude.txt
-Echo \skin.confluence\media\>>exclude.txt
+Echo \skin.embermod\media\>>exclude.txt
 Echo exclude.txt>>exclude.txt
 
 ECHO ----------------------------------------
 ECHO Creating XBT File...
-START /B /WAIT ..\..\Tools\TexturePacker\TexturePacker -dupecheck -input media -output ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.confluence\media\Textures.xbt
+START /B /WAIT ..\..\Tools\TexturePacker\TexturePacker -dupecheck -input media -output ..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.embermod\media\Textures.xbt
 
 ECHO ----------------------------------------
 ECHO XBT Texture Files Created...
 ECHO Building Skin Directory...
-xcopy "..\skin.confluence" "..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.confluence" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "..\skin.embermod" "..\..\project\Win32BuildSetup\BUILD_WIN32\application\addons\skin.embermod" /E /Q /I /Y /EXCLUDE:exclude.txt
 
 del exclude.txt
